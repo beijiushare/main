47abde27a5671493b8643b59305af30ee0aad5d6
beijiushare <1292396120@qq.com>
2026-07-14 01:45:18 +0800

    chore: 提交当前进度，准备改用 shadcn CLI 安装组件

--- 资源项/nyxui-main/app/components/layout.tsx
+++
@@ -1,9 +0,0 @@
-import SharedSidebarLayout from "@/components/global/SharedLayout";
-
-export default function ComponentsLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <SharedSidebarLayout>{children}</SharedSidebarLayout>;
-}
